bb5b70ea35c960365a70f3bc000780f2468d3e7b
tiago.franca <tiago.franca@solutis.com.br>
2018-09-28 15:21:00 -0300

    Ajustes iniciais no site

--- PROJECT/SITE/SerieList/src/App.vue
+++ PROJECT/SITE/SerieList/src/App.vue
@@ -1,23 +1,101 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="page-container">
+    <md-app md-waterfall md-mode="fixed-last">
+      <md-app-toolbar class="md-large md-dense md-primary">
+        <div class="md-toolbar-row">
+          <div class="md-toolbar-section-start">
+            <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+              <md-icon>menu</md-icon>
+            </md-button>
+
+            <span class="md-title">My Title</span>
+          </div>
+
+          <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button">
+              <md-icon>more_vert</md-icon>
+            </md-button>
+          </div>
+        </div>
+
+        <div class="md-toolbar-row">
+          <md-tabs>
+            <md-tab id="tab-home" md-label="Home"></md-tab>
+            <md-tab id="tab-pages" md-label="Pages"></md-tab>
+            <md-tab id="tab-posts" md-label="Posts"></md-tab>
+            <md-tab id="tab-favorites" md-label="Favorites"></md-tab>
+          </md-tabs>
+        </div>
+      </md-app-toolbar>
+
+      <md-app-drawer :md-active.sync="menuVisible">
+        <md-toolbar class="md-primary" md-elevation="0">Navigation</md-toolbar>
+
+        <md-list>
+          <md-list-item>
+            <md-icon>move_to_inbox</md-icon>
+            <span class="md-list-item-text">Inbox</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>send</md-icon>
+            <span class="md-list-item-text">Sent Mail</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>delete</md-icon>
+            <span class="md-list-item-text">Trash</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>error</md-icon>
+            <span class="md-list-item-text">Spam</span>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
+
+      <md-app-content>
+        <router-view/>
+      </md-app-content>
+    </md-app>
+    <md-toolbar class="md-medium md-primary footer">
+      <div class="md-toolbar-row">
+        <h3 class="md-title">Medium</h3>
+      </div>
+    </md-toolbar>
   </div>
 </template>
 
+<style lang="scss" scoped>
+  .md-app {
+    height: 100%;
+    // max-height: 400px;
+    border: 1px solid rgba(#000, .12);
+  }
+  .md-drawer {
+    width: 20%;
+    max-width: calc(100vw - 125px);
+  }
+  .footer{
+    
+  }
+  @import "~vue-material/dist/theme/engine";
+
+  @include md-register-theme("default", (
+    primary: #283593 ,
+    accent: #ffffff,
+    theme: light
+  ));
+
+@import "~vue-material/dist/theme/all";
+
+</style>
+
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data: () => ({
+    menuVisible: false
+  })
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
